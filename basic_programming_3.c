d771dc8cba0a49c5069d70a0a56266d01646a419
Yongggwon <101536895+Yongggwon@users.noreply.github.com>
2022-03-18 10:28:19 +0900

    basic_programming week_3

---
+++ basic_programming_3.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int num = 1;
+	int dan = 2;
+
+	while (dan < 10) 
+	{
+		while (num < 10) {
+			printf("%dx%d=%d  ", dan, num, dan * num);
+			num++;
+		}
+		num = 1;
+		printf("\n");
+		dan++;
+	}
+	return 0;
+}
