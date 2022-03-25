c23b497079b3a9d412bbd0232b10f181ce97ee6b
Yongggwon <101536895+Yongggwon@users.noreply.github.com>
2022-03-25 10:47:45 +0900

    Add files via upload
    subproject2

---
+++ basic_programming4_2.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <limits.h>
+#include <float.h>		//declare <limits.h>, <float.h> to use min, max
+
+int main()
+{
+	printf("char의 최대, 최소 : %d, %d\n",CHAR_MAX, CHAR_MIN);
+	printf("int의 최대, 최소 : %d, %d\n", INT_MAX, INT_MIN);
+	printf("double의 최대, 최소 : %d, %d\n", DBL_MAX, DBL_MIN);
+	printf("float의 최대, 최소 : %d, %d\n", FLT_MAX, FLT_MIN);		//use _MAX, _MIN to print each of char, int, double, float's maximum and minimum value
+	return 0;
+}
