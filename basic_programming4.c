a9d1e14924b7c442e364496d0cb8306e41d39932
Yongggwon <101536895+Yongggwon@users.noreply.github.com>
2022-03-25 10:26:01 +0900

    Add files via upload

---
+++ basic_programming4.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int num1;
+	int num2;
+	printf("input number a, b:");							//text that requiring input a and b
+	scanf_s("%d %d", &num1, &num2);							//we can input a and b what we want to calculate
+	printf("%d + %d = %d\n", num1, num2, num1 + num2);		//+
+	printf("%d - %d = %d\n", num1, num2, num1 - num2);		//-
+	printf("%d x %d = %d\n", num1, num2, num1 * num2);		//*
+
+	return 0;
+}
