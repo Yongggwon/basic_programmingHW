bf8c5529d6dbf8af268e9f6e2fd0e56996e2db7a
Yongggwon <101536895+Yongggwon@users.noreply.github.com>
2022-04-01 11:55:31 +0900

    Add files via upload

---
+++ basic_programming5_2.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	char x = 0;		
+	char y = 0;			//declare two variables what we want to calculate
+	double n = 0;
+	double m = 0;		//sice we cannot calculate text data, declare variale 'double' to change text to number
+
+	printf("두 수를 입력하세요:");		
+	scanf_s("%c", &x);
+	scanf_s(" %c", &y);		//input two number to calculate
+	n = x-48;
+	m = y-48;				//by ascii code, we can take '-48' to text to change for number data
+	printf("덧셈: %f\n", n + m);		//addition
+	printf("뺄셈: %f\n", n - m);		//subtraction
+	printf("곱셈: %f\n", n * m);		//multiplication
+	printf("나눗셈: %f\n", n/ m);		//division
+
+	return 0;
+}
---
+++ basic_programming_5.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	char x=0;		//declare x : what we want to change to capital
+	int i = 0;   //declare integer i to count number of executions
+	while (i < 5)    //반복문 선언: 5번 반복
+	{
+		printf("소문자 입력:");
+		scanf_s(" %c", &x);	//input small letter alphabet 
+		if (x >= 'a' && x <= 'z')  //조건문 선언: 입력한 값이 소문자여야 함
+		{
+			printf("대문자 : %c\n", x - 32);  //by ascii code, capital's code value is 'small letter - 32'
+			i++;   //counting number of executions
+		}
+	}
+	return 0;
+}
