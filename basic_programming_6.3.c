f9bd42cf0cc5c072391602bf9e2ce67b950f2540
Yongggwon <101536895+Yongggwon@users.noreply.github.com>
2022-04-09 12:33:50 +0900

    Add files via upload

---
+++ basic_programming_6.3.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//Second Operation
+int main()
+{
+	int i;
+	srand(time(NULL));		//시간을 초단위로 출력, seed값이 매초마다 바뀜
+	for (i = 0; i < 10; i++)		//repeat 10 time; print rand()
+		printf("%d\n", rand());
+}
