bddaed1c856c26568cf13510b1d915fffdbd344c
Yongggwon <101536895+Yongggwon@users.noreply.github.com>
2022-05-19 01:43:44 +0900

    Add files via upload

---
+++ basicprogramming_10.1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define SIZEx 4		
+#define SIZEy 4		//배열의 크기 지정
+
+void change(int a, int b)		//배열 두개를 매개변수로 받는 함수이다.
+{
+	int z, i;		//z는 배열의 원소교환을 위한 제 3의 정수형 변수
+	int* pa = a, * pb = b;		//입력받은 배열 두 개를 각각 포인터변수로 지정
+
+	if (SIZEx == SIZEy)		//배열의 사이즈가 같아야만 함수 실행
+	{
+		for (i = 0; i < SIZEx; i++)	//배열의 크기만큼 반복
+		{
+			z = *(pa + i);	//a를 z로, b를 a로, z를 b로 저장하면 a, b맞교환됨
+			*(pa + i) = *(pb + i);
+			*(pb + i) = z;		//+i를 통해 각 배열의 index가 같은 원소끼리 교환함
+		}
+		printf("바뀐 배열 a는 %d %d %d %d \n", *(pa), *(pa + 1), *(pa + 2), *(pa + 3));
+		printf("바뀌 배열 b는 %d %d %d %d \n", *(pb), *(pb + 1), *(pb + 2), *(pb + 3));		//바뀐 배열 출력
+	}
+	else
+		printf("배열의 크기가 다릅니다.");		//사이즈가 다르면 출력할 메세지
+}
+int main(void)
+{
+	int a[SIZEx] = { 1,2,3,4 };
+	int b[SIZEy] = { 5,6,7,8 };
+
+	change(a, b);
+}
---
+++ basicprogramming_10.2.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#define SIZEa 2		//첫 행렬의 행
+#define SIZEb 3		//첫 행렬의 열
+#define SIZEc 3		//두 번째 행렬의 행
+#define SIZEd 3		//두 번째 행렬의 열
+
+void matrix_product(int *pa, int *pb)		//입력받은 행렬을 포인터 배열로 입력받음
+{
+	int i, j,k, result = 0;		//행렬곱을 
+	int* elt = &result;			//결과값을 포인터 변수로 지정
+
+	if (SIZEb == SIZEc)			//행렬곱이 실행되기 위한 조건	
+	{
+		for (i = 0; i < SIZEa; i++)		//행에 대한 연산
+		{
+
+			for (j = 0; j < SIZEd; j++)		//열에 대한 연산
+			{
+				*(elt+i*j) = 0;			//포인터 변수를 초기화
+				
+				for (k = 0; k < SIZEb; k++)			//첫 행렬의 행과 두번째 행렬의 열을 곱한 값을 각각 더하기 위함
+				{
+					result += *(pa + SIZEa * i + k) * *(pb + k * SIZEd + j);
+				}
+				printf("%d ", result);		//결과값 출력
+				
+			}
+			printf("\n");		//1행의 출력이 끝나면 2행 출력
+		}
+		printf("\n");
+	}
+	else
+		printf("계산불가");		//조건을 만족하지 않을 시 계산 불가
+}
+
+int main(void)
+{
+	int A[SIZEa * SIZEb] = { 3, 1, 3,    4, 5, 1 };
+	int B[SIZEc * SIZEd] = { 2,4,6,   1,2,3,   6,5,4 };
+
+	matrix_product(A, B);
+	return 0;
+}
