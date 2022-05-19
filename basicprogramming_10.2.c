71198004bed14aa0fdfcb9fe0987f6ddc754ad88
Yongggwon <101536895+Yongggwon@users.noreply.github.com>
2022-05-19 15:37:00 +0900

    Update basicprogramming_10.2.c

--- basicprogramming_10.2.c
+++ basicprogramming_10.2.c
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #define SIZEa 2		//첫 행렬의 행
 #define SIZEb 3		//첫 행렬의 열
 #define SIZEc 3		//두 번째 행렬의 행
@@ -16,13 +16,13 @@ void matrix_product(int *pa, int *pb)		//입력받은 행렬을 포인터 배열
 
 			for (j = 0; j < SIZEd; j++)		//열에 대한 연산
 			{
-				*(elt+i*j) = 0;			//포인터 변수를 초기화
-				
+				result = 0;		//result 변수 초기화
 				for (k = 0; k < SIZEb; k++)			//첫 행렬의 행과 두번째 행렬의 열을 곱한 값을 각각 더하기 위함
 				{
-					result += *(pa + SIZEa * i + k) * *(pb + k * SIZEd + j);
+					result += *(pa + SIZEb * i + k) * *(pb + k * SIZEd + j);	//result에 결과값 저장
 				}
-				printf("%d ", result);		//결과값 출력
+				*(elt + i + SIZEd * j)=result;
+				printf("%d ", *(elt + i + SIZEd * j));		//결과값 출력
 				
 			}
 			printf("\n");		//1행의 출력이 끝나면 2행 출력
@@ -35,8 +35,8 @@ void matrix_product(int *pa, int *pb)		//입력받은 행렬을 포인터 배열
 
 int main(void)
 {
-	int A[SIZEa * SIZEb] = { 3, 1, 3,    4, 5, 1 };
-	int B[SIZEc * SIZEd] = { 2,4,6,   1,2,3,   6,5,4 };
+	int A[SIZEa * SIZEb] = {1,2,3,4,5,6 };
+	int B[SIZEc * SIZEd] = {0,1,2,3,4,5,4,3,2};
 
 	matrix_product(A, B);
 	return 0;
